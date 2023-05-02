5dede9dc188ffd77bb3d66dcc74d4b0e329e0145
Mandar <mandar.athavale@gmail.com>
2023-05-02 14:24:30 +0530

    Update ESG_Mandar.py

--- ESG_Mandar.py
+++ ESG_Mandar.py
@@ -19,8 +19,8 @@ def init_connection():
     return snowflake.connector.connect(
         **st.secrets["SNOWFLAKEPOC"], client_session_keep_alive=True
     )
-
-conn = init_connection()
+    conn = init_connection()
+    return conn
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
@@ -32,12 +32,21 @@ def run_query(query):
 
 run_query("USE ROLE SYSADMIN;")
 
-#esg_raw_data  = run_query("select csrhub_id,company_name,isin,ticker,industry_desc,dj30tag,community,employees,environment,governance,rating_date,rating_status from csrhub.public.faststarttrial ;")
-esg_raw_df = pd.read_sql("SELECT * FROM csrhub.public.faststarttrial;", conn)
+@st.cache_data
+def get_raw_esg_data(conn):
+    esg_raw_df = pd.read_sql("SELECT * FROM csrhub.public.faststarttrial;", conn)
+    return esg_raw_df
 
-pd_esg_raw_df = pd.DataFrame(esg_raw_df)
-pd_group_score_df = pd_esg_raw_df[['INDUSTRY_DESC','COMMUNITY','EMPLOYEES','ENVIRONMENT','GOVERNANCE']]
-#pd_rows = pd_rows.groupby(["EMPID"]).sum()
-pd_group_score_df = pd_group_score_df.groupby('INDUSTRY_DESC',as_index = False)[['COMMUNITY','EMPLOYEES','ENVIRONMENT','GOVERNANCE']].mean()
 
+def get_agg_esg_data(conn):
+    esg_raw_df = get_raw_esg_data(conn)
+    pd_esg_raw_df = pd.DataFrame(esg_raw_df)
+    pd_group_score_df = pd_esg_raw_df[['INDUSTRY_DESC','COMMUNITY','EMPLOYEES','ENVIRONMENT','GOVERNANCE']]
+    pd_group_score_df = pd_group_score_df.groupby('INDUSTRY_DESC',as_index = False)[['COMMUNITY','EMPLOYEES','ENVIRONMENT','GOVERNANCE']].mean()
+    return pd_group_score_df
+
+
+try:
+    init_connection()
+    get_raw_esg_data(_session)
 st.dataframe(pd_group_score_df)
